2bcea722ab32bf7d1d7485b755300342b764a4ba
Tope79 <65842132+Tope79@users.noreply.github.com>
2020-05-24 22:26:04 -0500

    Add files via upload

---
+++ Untitled21.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# This notebook will be mainly used for Capstone project
+import pandas as pd
+import Numpy as np
+print("Hello Capstone Project!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
